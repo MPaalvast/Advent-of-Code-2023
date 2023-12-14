7fa5dd05b4c36ed12e8b55cccd423ea41382e309
MPaalvast <martijn_paalvast@hotmail.com>
2023-12-14 22:34:20 +0100

    day 14 done

--- src/Service/Days/Day14.php
+++ src/Service/Days/Day14.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Day14 extends AbstractController
 {
-    public function __construct(public array $grid = [], public int $result = 0)
+    public function __construct(public array $grid = [], public int $maxRows = 0, public int $maxCol = 0)
     {
     }
 
@@ -14,9 +14,8 @@ class Day14 extends AbstractController
     {
         $this->createGrid($rows);
         $this->moveRocksUp();
-        $this->calculateResult();
 
-        return $this->result;
+        return $this->calculateResult();
     }
 
     private function createGrid($rows): void
@@ -28,126 +27,109 @@ class Day14 extends AbstractController
             }
             $this->grid[] = str_split($row);
         }
+        $this->maxRows = count($this->grid);
+        $this->maxCol = count($this->grid[0]);
     }
-
-//    private function moveRocksUp(): void
-//    {
-//        // while kan eruit
-//        // kijk per 0 hoever hij kan verplaatsen en verplaats hem
-//        $moved = true;
-//        while ($moved) {
-//            $moved = false;
-//            foreach ($this->grid as $gridRowNr => $gridRow) {
-//                if ($gridRowNr === 0) {
-//                    continue;
-//                }
-//                foreach ($gridRow as $fieldNr => $fieldValue) {
-//                    if ($fieldValue === 'O' && $this->grid[$gridRowNr-1][$fieldNr] === '.') {
-//                        $this->grid[$gridRowNr-1][$fieldNr] = 'O';
-//                        $this->grid[$gridRowNr][$fieldNr] = '.';
-//                        $moved = true;
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-//    private function moveRocksUp(): void
-//    {
-//        foreach ($this->grid as $gridRowNr => $gridRow) {
-//            foreach ($gridRow as $fieldNr => $fieldValue) {
-//                if ($this->grid[$gridRowNr][$fieldNr] === '.') {
-//                    $test = '1';
-//                }
-//            }
-//        }
-//    }
 
     private function moveRocksUp(): void
     {
-        // while kan eruit
-        // kijk per 0 hoever hij kan verplaatsen en verplaats hem
-//        $moved = true;
-//        while ($moved) {
-//            $moved = false;
-//        dump($this->grid);
         foreach ($this->grid as $gridRowNr => $gridRow) {
             if ($gridRowNr === 0) {
                 continue;
             }
-
-            // get values and keys where de value = O
             $positionsToCheck = array_keys($gridRow, "O");
-            dump($this->grid);
-            if (count($positionsToCheck) > 0) {
-                foreach ($positionsToCheck as $y) {
-                    $i = 1;
-                    if ($this->grid[$gridRowNr-$i][$y] !== '.') {
-                        continue;
-                    }
-                    while ($gridRowNr-$i > 0 && isset($this->grid[$gridRowNr-$i][$y]) && $this->grid[$gridRowNr-$i][$y] === '.') {
-                        $i++;
-                    }
-                    dump($gridRowNr-$i . '----');
-                    $this->grid[$gridRowNr-$i][$y] = 'O';
-                    $this->grid[$gridRowNr][$y] = '.';
+            foreach ($positionsToCheck as $fieldNr) {
+                $i=-1;
+                do {
+                    $i++;
+                } while (isset($this->grid[$gridRowNr-($i+1)]) && $this->grid[$gridRowNr-($i+1)][$fieldNr] === '.');
+                if ($i>0) {
+                    $this->grid[$gridRowNr-$i][$fieldNr] = 'O';
+                    $this->grid[$gridRowNr][$fieldNr] = '.';
                 }
-                dump($this->grid);
             }
-
-//            foreach ($gridRow as $fieldNr => &$fieldValue) {
-//                if ($fieldValue === 'O') {
-//                    $hasChanged = false;
-//                    $i=0;
-//                    while (!$hasChanged) {
-////                        dump($gridRowNr . '-' . $i . '===');
-////                        dump($this->grid[$gridRowNr-($i+1)][$fieldNr]);
-//                        if (!isset($this->grid[$gridRowNr-($i+1)][$fieldNr]) || in_array($this->grid[$gridRowNr-($i+1)][$fieldNr], ['#', 'O'], true)) {
-////                            dump('---');
-//                            $this->grid[$gridRowNr-$i][$fieldNr] = 'O';
-//                            $fieldValue = '.';
-//                            $hasChanged = true;
-//                            break 2;
-//                        }
-//
-//                        $i++;
-//                    }
-//                }
-//            }
         }
-//        }
     }
 
-    private function calculateResult(): void
+    private function calculateResult(): string
     {
+        $result = 0;
         $gridTotalRowValue = count($this->grid);
         foreach ($this->grid as $gridRow) {
             $rowCounts = count(array_keys($gridRow, "O"));
             if ($rowCounts > 0) {
-                $this->result += $rowCounts * $gridTotalRowValue;
+                $result += $rowCounts * $gridTotalRowValue;
             }
             $gridTotalRowValue--;
         }
+
+        return $result;
     }
 
     public function generatePart2($rows): string
     {
-        $totalCycles = 1000000000;
-////        $this->createGrid($rows);
-//        for ($i=1;$i<=$totalCycles;$i++) {
-////            $this->moveRocksUp();
-////            $this->moveRocksLeft();
-////            $this->moveRocksDown();
-////            $this->moveRocksRight();
-//        }
+        $this->createGrid($rows);
+        $totalCycles = 500;
+        $y = 0;
+        $testArray[$y] = [];
         $i = 0;
         do {
             $this->moveRocksUp();
-//            $this->moveRocksLeft();
-//            $this->moveRocksDown();
-//            $this->moveRocksRight();
+            $this->rotate();
+            $this->moveRocksUp();
+            $this->rotate();
+            $this->moveRocksUp();
+            $this->rotate();
+            $this->moveRocksUp();
+            $this->rotate();
             $i++;
+            $result = $this->calculateResult();
+            if (in_array($result, $testArray[$y], true)) {
+                $y++;
+            }
+            $testArray[$y][] = $result;
         } while ($i<=$totalCycles);
-        return 0;
+
+        $loop = true;
+        $i = 0;
+        $cycles = 1000000000;
+        while ($loop) {
+            if (implode(',', $testArray[$i]) === implode(',', $testArray[$i+2])) {
+                $resultArrayValues = explode(',', implode(',', $testArray[$i]) . ',' . implode(',', $testArray[$i+1]));
+                $loop = false;
+            } else {
+                $cycles -= count($testArray[$i]);
+                $i++;
+            }
+        }
+
+        $position = $cycles%count($resultArrayValues);
+
+        return ($resultArrayValues[$position-1]);
+    }
+
+    private function dumpGrid(): void
+    {
+        $dump = '';
+        foreach ($this->grid as $row) {
+            $dump .= implode('', $row) . "\n";
+        }
+        dump($dump);
+    }
+
+    private function rotate(): void
+    {
+        $newGrid = [];
+
+        foreach ($this->grid as $gridRowKey => $gridRow) {
+            foreach ($gridRow as $fieldKey => $field) {
+                $newGrid[$fieldKey][$gridRowKey] = $field;
+            }
+        }
+        foreach ($newGrid as $key => $row) {
+            $newGrid[$key] = array_reverse($row);
+        }
+
+        $this->grid = $newGrid;
     }
 }
