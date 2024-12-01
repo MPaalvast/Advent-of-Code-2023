6885021e0936bcf390161eaeb64cb9d6094d79de
MPaalvast <martijn_paalvast@hotmail.com>
2024-12-01 10:09:11 +0100

    day1 part1 completed

--- src/Service/Days/Year2024/Y2024D1Service.php
+++ src/Service/Days/Year2024/Y2024D1Service.php
@@ -6,7 +6,7 @@ use App\Service\Days\DayServiceInterface;
 
 class Y2024D1Service implements DayServiceInterface
 {
-    private string $title = "???";
+    private string $title = "Historian Hysteria";
 
     public function getTitle(): string
     {
@@ -15,7 +15,33 @@ class Y2024D1Service implements DayServiceInterface
 
     public function generatePart1(array|\Generator $rows): string
     {
-        return 0;
+        // explode row on 3 spaces (any spaces with regex) and make 2 arrays
+        // order both arrays asc and loop over them to calculate the diff
+        // keep a total of the diff (array_sum())
+        $leftArray = [];
+        $rightArray = [];
+
+        foreach ($rows as $row) {
+            [$left, $right] = explode("   ", $row);
+            $leftArray[] = $left;
+            $rightArray[] = $right;
+        }
+
+        $totalDiff = $this->getDiff($leftArray, $rightArray);
+
+        return $totalDiff;
+    }
+
+    private function getDiff($leftValueArray, $rightValueArray): int
+    {
+        $totalDiff = 0;
+        sort($leftValueArray);
+        sort($rightValueArray);
+        foreach ($leftValueArray as $key => $leftValue) {
+            $totalDiff += abs($leftValue - $rightValueArray[$key]);
+        }
+
+        return $totalDiff;
     }
 
     public function generatePart2(array|\Generator $rows): string
