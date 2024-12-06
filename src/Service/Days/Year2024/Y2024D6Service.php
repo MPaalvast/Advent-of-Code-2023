2cd057609c10263f2e216c9a31a22dc1cc285ac5
MartijnPaalvast <m.paalvast@paragin.com>
2024-12-06 13:11:52 +0100

    Day6 part 2: Work in progress

--- src/Service/Days/Year2024/Y2024D6Service.php
+++ src/Service/Days/Year2024/Y2024D6Service.php
@@ -23,6 +23,7 @@ class Y2024D6Service implements DayServiceInterface
     private string $currentDirection = '';
 
     private array $blockingFields = [];
+    private array $loopingFields = [];
 
     private array $nextDirection = [
         'up' => 'right',
@@ -48,6 +49,17 @@ class Y2024D6Service implements DayServiceInterface
 
     public function generatePart2(array|\Generator $rows): string
     {
+        // same as part 1
+        // but remember the direction of the fields
+            // maybe it can have 2 directions???
+        // when the next field is not #
+            // check the right path for the current position until you see a # or a field with the same direction as you check
+                // the next field in the current direction is an obstruction
+
+        $this->generateGrid($rows);
+        $this->move(true);
+        $this->calculateLoopOptions();
+
         return $this->total;
     }
 
@@ -67,13 +79,13 @@ class Y2024D6Service implements DayServiceInterface
             foreach ($gridRow as $y => $rowYValue) {
                 if (in_array($rowYValue, ['^', '<', '>', 'v'])) {
                     $this->currentPoint = ['x' => $x, 'y' => $y];
-                    $this->visitedFields[$x . '-' . $y] = ['x' => $x, 'y' => $y];
                     $this->currentDirection = match ($rowYValue) {
                         '^' => 'up',
                         '<' => 'left',
                         '>' => 'right',
                         'v' => 'down',
                     };
+                    $this->visitedFields[$x . '-' . $y] = $this->currentDirection;
                 }
                 if ($rowYValue === '#') {
                     $this->blockingFields[$x . '-' . $y] = ['x' => $x, 'y' => $y];
@@ -85,12 +97,12 @@ class Y2024D6Service implements DayServiceInterface
         $this->maxXIndex = count($this->grid);
     }
 
-    private function move(): void
+    private function move(bool $findLoops = false): void
     {
         $currentX = $this->currentPoint['x'];
         $currentY = $this->currentPoint['y'];
         while (true) {
-            [$nextX, $nextY] = $this->getNextCoordinates($currentX, $currentY);
+            [$nextX, $nextY] = $this->getNextCoordinates($currentX, $currentY, $this->currentDirection);
             if (!isset($this->grid[$nextX][$nextY])) {
                 // moved of the grid
                 break;
@@ -103,19 +115,54 @@ class Y2024D6Service implements DayServiceInterface
             }
 
             $nextIndex = $nextX . '-' . $nextY;
+
+            if ($findLoops && $this->findLoop()) {
+                $this->loopingFields[] = $nextIndex;
+            }
+
             if (!isset($this->visitedFields[$nextIndex])) {
                 // add to the visited fields en move currentPoint to the new location
-                $this->visitedFields[$nextIndex] = ['x' => $nextX, 'y' => $nextY];
-                $this->currentPoint = ['x' => $nextX, 'y' => $nextY];
+                $this->visitedFields[$nextIndex] = $this->currentDirection;
+                // TODO: fix multiple directions
+                // i think this has to be an array with multiple directions
             }
+            $this->currentPoint = ['x' => $nextX, 'y' => $nextY];
             $currentX = $nextX;
             $currentY = $nextY;
         }
     }
 
-    private function getNextCoordinates(int $x, int $y): array
+    private function findLoop(): bool
     {
-        match ($this->currentDirection) {
+         $direction = $this->currentDirection;
+         $currentX = $this->currentPoint['x'];
+         $currentY = $this->currentPoint['y'];
+         $directionToCheck = $this->nextDirection[$direction];
+         while (true) {
+             [$nextX, $nextY] = $this->getNextCoordinates($currentX, $currentY, $directionToCheck);
+             if (!isset($this->grid[$nextX][$nextY]) || $this->grid[$nextX][$nextY] === '#') {
+                 return false;
+             }
+
+             // TODO: This has to be turned on when fixed -> fix multiple directions
+//             if ($this->grid[$nextX][$nextY] === '#') {
+//                 // turn to the next direction
+//                 $directionToCheck = $this->nextDirection[$directionToCheck];
+//                 continue;
+//             }
+
+             $nextIndex = $nextX . '-' . $nextY;
+             if (isset($this->visitedFields[$nextIndex]) && $this->visitedFields[$nextIndex] === $directionToCheck) {
+                 return true;
+             }
+             $currentX = $nextX;
+             $currentY = $nextY;
+         }
+    }
+
+    private function getNextCoordinates(int $x, int $y, string $direction): array
+    {
+        match ($direction) {
             'up' => --$x,
             'down' => ++$x,
             'left' => --$y,
@@ -129,4 +176,10 @@ class Y2024D6Service implements DayServiceInterface
     {
         $this->total = count($this->visitedFields);
     }
+    private function calculateLoopOptions(): void
+    {
+        $this->total = count($this->loopingFields);
+    }
+
+    // Part2: result 578 to low
 }
