169b56eea2ee94808ace0ffba94d1d028fc905eb
MPaalvast <martijn_paalvast@hotmail.com>
2024-12-07 19:10:13 +0100

    Day 7 parts Done

--- src/Service/Days/Year2024/Y2024D7Service.php
+++ src/Service/Days/Year2024/Y2024D7Service.php
@@ -2,13 +2,18 @@
 
 namespace App\Service\Days\Year2024;
 
+use AllowDynamicProperties;
 use App\Service\Days\DayServiceInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 
-#[AsTaggedItem('Y2024D7')]
+#[AllowDynamicProperties] #[AsTaggedItem('Y2024D7')]
 class Y2024D7Service implements DayServiceInterface
 {
-    private string $title = "???";
+    private string $title = "Bridge Repair";
+
+    private int $total = 0;
+
+    private int $expectedValue = 0;
 
     public function getTitle(): string
     {
@@ -17,11 +22,90 @@ class Y2024D7Service implements DayServiceInterface
 
     public function generatePart1(array|\Generator $rows): string
     {
-        return 0;
+        $this->getCorrectResults($rows);
+
+        return $this->total;
     }
 
     public function generatePart2(array|\Generator $rows): string
     {
-        return 0;
+        $this->getCorrectResults($rows, true);
+
+        return $this->total;
+    }
+
+    private function getCorrectResults(array|\Generator $rows, bool $canMerge = false): void
+    {
+        foreach ($rows as $x => $row) {
+            $row = trim(preg_replace('/\r+/', '', $row));
+            if (empty($row)) {
+                continue;
+            }
+
+            [$this->expectedValue, $stringFields] = explode(": ", $row);
+            $valuesToCalculate = explode(" ", $stringFields);
+
+            if ($canMerge) {
+                if ($this->getCorrectPlusTimesMergeResults($valuesToCalculate)) {
+                    $this->total += $this->expectedValue;
+                }
+            } else {
+                if ($this->checkPlusTimesResult($valuesToCalculate)) {
+                    $this->total += $this->expectedValue;
+                }
+            }
+        }
+    }
+
+    private function checkPlusTimesResult(array $valuesToCalculate, int $currentValue = 0): bool
+    {
+        $value = array_shift($valuesToCalculate);
+        if (empty($valuesToCalculate)) {
+            if (
+                $value+$currentValue === $this->expectedValue
+                || $value*$currentValue === $this->expectedValue
+            ) {
+                return true;
+            }
+
+            return false;
+        }
+
+        if ($this->checkPlusTimesResult($valuesToCalculate, $value+$currentValue)
+            || $this->checkPlusTimesResult($valuesToCalculate, $value*$currentValue)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getCorrectPlusTimesMergeResults(array $valuesToCalculate, int $currentValue = 0): bool
+    {
+        $value = array_shift($valuesToCalculate);
+        if (empty($valuesToCalculate)) {
+            if (
+                $value*$currentValue === $this->expectedValue
+                || $value+$currentValue === $this->expectedValue
+                || sprintf("%d%d", $currentValue, $value) === (string)$this->expectedValue
+            ) {
+                return true;
+            }
+
+            return false;
+        }
+
+        $mergeString = $value;
+        if ($currentValue !== 0) {
+            $mergeString = sprintf("%d%d", $currentValue, $value);
+        }
+        if (
+            $this->getCorrectPlusTimesMergeResults($valuesToCalculate, (int)$mergeString)
+            || $this->getCorrectPlusTimesMergeResults($valuesToCalculate, bcadd($value, $currentValue))
+            || $this->getCorrectPlusTimesMergeResults($valuesToCalculate, bcmul($value, $currentValue))
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
