ff3d57820927b8e6c8a2ead97242bab569b8e3d7
MPaalvast <martijn_paalvast@hotmail.com>
2024-12-08 11:05:26 +0100

    Day 8 code geoptimaliseerd en duplucates eruit gehaald

--- src/Service/Days/Year2024/Y2024D8Service.php
+++ src/Service/Days/Year2024/Y2024D8Service.php
@@ -74,22 +74,23 @@ class Y2024D8Service implements DayServiceInterface
         $this->total = count($this->foundNodes);
     }
 
+    private function getDiffCoordinates(array $pointA, array $pointB): array
+    {
+        return [
+            'x' => $pointA['x'] - $pointB['x'],
+            'y' => $pointA['y'] - $pointB['y'],
+        ];
+    }
+
     private function findTotalNodes(array $frequencyArray): void
     {
         while (!empty($frequencyArray)) {
             $node = array_shift($frequencyArray);
             foreach ($frequencyArray as $frequencyPoint) {
-                $diffX = $node['x'] - $frequencyPoint['x'];
-                $diffY = $node['y'] - $frequencyPoint['y'];
+                $diff = $this->getDiffCoordinates($node, $frequencyPoint);
 
-                $this->isValidNode(
-                    $node['x'] + $diffX,
-                    $node['y'] + $diffY
-                );
-                $this->isValidNode(
-                    $frequencyPoint['x'] + ($diffX >= 0 ? ($diffX * -1) : abs($diffX)),
-                    $frequencyPoint['y'] + ($diffY >= 0 ? ($diffY * -1) : abs($diffY))
-                );
+                $this->findValidNodesLoop($node, $diff, false, false);
+                $this->findValidNodesLoop($frequencyPoint, $diff, true, false);
             }
         }
     }
@@ -99,10 +100,7 @@ class Y2024D8Service implements DayServiceInterface
         while (!empty($frequencyArray)) {
             $node = array_shift($frequencyArray);
             foreach ($frequencyArray as $frequencyPoint) {
-                $diff = [
-                    'x' => $node['x'] - $frequencyPoint['x'],
-                    'y' => $node['y'] - $frequencyPoint['y']
-                ];
+                $diff = $this->getDiffCoordinates($node, $frequencyPoint);
 
                 $this->findAntinodes($node, $diff);
                 $this->findAntinodes($frequencyPoint, $diff);
@@ -112,24 +110,27 @@ class Y2024D8Service implements DayServiceInterface
 
     private function findAntinodes(array $node, array $diff): void
     {
-        $x = $node['x'];
-        $y = $node['y'];
-        while (true) {
-            $calculatedX = $x + $diff['x'];
-            $calculatedY = $y + $diff['y'];
-            if (!$this->isValidNode($calculatedX, $calculatedY)) {
-                break;
-            }
-            $x = $calculatedX;
-            $y = $calculatedY;
-        }
+        $this->findValidNodesLoop($node, $diff);
+        $this->findValidNodesLoop($node, $diff, true);
+    }
 
+    private function findValidNodesLoop(array $node, array $diff, bool $calculateAbs = false, bool $keepsLooping = true): void
+    {
         $x = $node['x'];
         $y = $node['y'];
         while (true) {
-            $calculatedX = $x + ($diff['x'] >= 0 ? ($diff['x'] * -1) : abs($diff['x']));
-            $calculatedY = $y + ($diff['y'] >= 0 ? ($diff['y'] * -1) : abs($diff['y']));
-            if (!$this->isValidNode($calculatedX, $calculatedY)) {
+            if ($calculateAbs) {
+                $calculatedX = $x + ($diff['x'] >= 0 ? ($diff['x'] * -1) : abs($diff['x']));
+                $calculatedY = $y + ($diff['y'] >= 0 ? ($diff['y'] * -1) : abs($diff['y']));
+            } else {
+                $calculatedX = $x + $diff['x'];
+                $calculatedY = $y + $diff['y'];
+            }
+
+            if (
+                !$this->isValidNode($calculatedX, $calculatedY) ||
+                !$keepsLooping
+            ) {
                 break;
             }
             $x = $calculatedX;
