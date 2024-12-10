948c0f19c3a2e5cd471980773d6a29f8cc5525d5
MPaalvast <martijn_paalvast@hotmail.com>
2024-12-10 17:27:22 +0100

    Day9 refactored but part 2 not working

--- src/Service/Days/Year2024/Y2024D9Service.php
+++ src/Service/Days/Year2024/Y2024D9Service.php
@@ -29,15 +29,16 @@ class Y2024D9Service implements DayServiceInterface
     {
         $this->getBlockAndFileArray($rows);
         $this->moveFileBlocks();
-        $this->calculateChecksum();
+        $this->calculateTotalChecksum();
 
         return $this->total;
     }
 
     public function generatePart2(array|\Generator $rows): string
     {
-        $this->getBlockAndFileArrayTotal($rows);
+        $this->getBlockAndFileArray($rows);
         $this->moveTotalFileBlocks();
+        $this->calculateTotalChecksum();
 
         return $this->total;
     }
@@ -49,43 +50,9 @@ class Y2024D9Service implements DayServiceInterface
             if (empty($row)) {
                 continue;
             }
-
             $parts = str_split($row);
             $fileIndex = 0;
             $i = 0;
-            foreach ($parts as $part) {
-                if ($i % 2 === 0) {
-                    // file
-                    for ($y=0; $y<(int)$part; $y++) {
-                        $this->blockAndFileArray[] = $fileIndex;
-                    }
-                    $fileIndex++;
-                } else {
-                    // space
-                    if ((int)$part > 0) {
-                        for ($y = 0; $y < (int)$part; $y++) {
-                            $this->blockAndFileArray[] = '.';
-                        }
-                    }
-                }
-
-                $i++;
-            }
-        }
-        dump($this->blockAndFileArray);
-    }
-
-    private function getBlockAndFileArrayTotal(array|\Generator $rows): void
-    {
-        foreach ($rows as $row) {
-            $row = trim(preg_replace('/\r+/', '', $row));
-            if (empty($row)) {
-                continue;
-            }
-            $parts = str_split($row);
-            $fileIndex = 0;
-            $i = 0;
-
             foreach ($parts as $part) {
                 if ($i % 2 === 0) {
                     // file
@@ -98,44 +65,71 @@ class Y2024D9Service implements DayServiceInterface
 
                 $i++;
             }
+            $this->blockArray[] = 0;
         }
-//        dump($this->fileArray);
-//        dump($this->blockArray);
     }
 
     private function moveFileBlocks(): void
     {
-        $frontArray = [];
-        $backArray = [];
-        $array = $this->blockAndFileArray;
+        $fileArrayReversed = array_reverse($this->fileArray);
+        foreach ($fileArrayReversed as $part) {
+            $fileId = key($part);
+            for ($i = 0; $i < $part[$fileId]; $i++) {
+                foreach ($this->blockArray as $key => $value) {
+                    if ($key >= $fileId) {
+                        break;
+                    }
+                    if ($value > 0) {
+                        $this->blockArray[$key]--;
+                        if (count($this->fileArray[$fileId]) > 1) {
+                            $this->blockArray[$fileId-1]++;
+                        } else {
+                            $this->blockArray[$fileId]++;
+                        }
+                        if (!isset($this->fileArray[$key][$fileId])) {
+                            $this->fileArray[$key] += [$fileId => 1];
+                        } else {
+                            $this->fileArray[$key][$fileId]++;
+                        }
 
-        while (!empty($array)) {
-            $frontChar = array_shift($array);
-            if ($frontChar !== '.') {
-                // voeg de waarde aan de start string toe
-                $frontArray[] = $frontChar;
-            } else {
-                // haal het laatste getal op in de $array
-                while (!empty($array)) {
-                    $backChar = array_pop($array);
-                    array_unshift($backArray, '.');
-                    if ($backChar !== '.') {
-                        $frontArray[] = $backChar;
+                        $this->fileArray[$fileId][$fileId]--;
+                        if ($this->fileArray[$fileId][$fileId] === 0) {
+                            unset($this->fileArray[$fileId][$fileId]);
+                        }
+
                         break;
                     }
                 }
             }
         }
-
-        $this->blockAndFileSortedArray = array_merge($frontArray, $backArray);
     }
 
     private function moveTotalFileBlocks(): void
     {
-//        $array = $this->blockAndFileArray;
-//        foreach (array_reverse($array) as $part) {
-//            array_search('');
-//        }
+        $fileArrayReversed = array_reverse($this->fileArray);
+
+        foreach ($fileArrayReversed as $part) {
+            $fileId = key($part);
+            $fileDiskSize = strlen($fileId) * $part[$fileId];
+
+            foreach ($this->blockArray as $key => $value) {
+                if ($key > $fileId) {
+                    break;
+                }
+                if ($value >= $fileDiskSize) {
+                    $this->blockArray[$key] -= $fileDiskSize;
+                    if (count($this->fileArray[$fileId]) > 1) {
+                        $this->blockArray[$fileId-1] += $fileDiskSize;
+                    } else {
+                        $this->blockArray[$fileId] += $fileDiskSize;
+                    }
+                    $this->fileArray[$key] += $part;
+                    unset($this->fileArray[$fileId][$fileId]);
+
+                    break;
+                }
+            }
+        }
     }
 
     private function calculateChecksum(): void
@@ -148,80 +142,28 @@ class Y2024D9Service implements DayServiceInterface
         }
     }
 
-//$input = 2333133121414131402;
-//
-//$fileIndex = 2313244342
-//$space = 333111110
-//
-//$test = [
-//0 => [
-//0 => 2
-//],
-//1 => [
-//1 => 3
-//],
-//2 => [
-//2 => 1
-//],
-//3 => [
-//3 => 3
-//],
-//4 => [
-//4 => 2
-//],
-//5 => [
-//5 => 4
-//],
-//6 => [
-//6 => 4
-//],
-//7 => [
-//7 => 3
-//],
-//8 => [
-//8 => 4
-//],
-//9 => [
-//9 => 2
-//],
-//]
-//
-//$test1 = [
-//0 => 2
-//1 => 3
-//2 => 1
-//3 => 3
-//4 => 2
-//5 => 4
-//6 => 4
-//7 => 3
-//8 => 4
-//9 => 2
-//]
-//
-//calculate count(index) * value
-//    // 123 => 4 = 3*4 = 12
-//    // 9 => 2 = 1*2 = 2
-//$calculatedValue = 2
-//
-//$index = array_find_key($testDot, function (int $value) use (int $calculatedValue) {
-//    return $value >= $calculatedValue;
-//});
-//
-//// $textDot[$index] -= $calculatedValue;
-//// $textDot[9] += $calculatedValue;
-//// $test[$index] = $test[$index] + [9 => 2] // <-- $test1
-//// unset($test[9][9])
-//
-//$testDot = [
-//    0 => 3,
-//    1 => 3,
-//    2 => 3,
-//    3 => 1,
-//    4 => 1,
-//    5 => 1,
-//    6 => 1,
-//    7 => 1,
-//    8 => 0,
-//]
+    private function calculateTotalChecksum(): void
+    {
+        $i = 0;
+        $string = '';
+        foreach ($this->fileArray as $index => $parts) {
+//            dump($parts);
+            if (!empty($parts)) {
+                foreach ($parts as $fileId => $amount) {
+                    for ($y = 0; $y < (int)$amount; $y++) {
+                        $string .= $fileId;
+                        $this->total += $i * (int)$fileId;
+                        $i++;
+                    }
+                }
+            }
+            $string .= str_repeat('.', $this->blockArray[$index]);
+            $i += $this->blockArray[$index];
+        }
+//        dump($string);
+    }
+
+// 14295934907371 => to high
+// 8187318443496 => to high
+// 7051071286497 => to high
 }
