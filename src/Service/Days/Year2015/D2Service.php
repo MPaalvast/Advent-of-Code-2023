a63ad132932da55b1ef46a64a655b0d9e725432e
MPaalvast <martijn_paalvast@hotmail.com>
2025-01-05 14:10:24 +0100

    Year 2015 Day 2 done

--- src/Service/Days/Year2015/D2Service.php
+++ src/Service/Days/Year2015/D2Service.php
@@ -9,20 +9,109 @@ use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 class D2Service implements DayServiceInterface
 {
     private int $total = 0;
+    private array $packages = [];
 
     public function generatePart1(array $rows): string
     {
+        $this->initInput($rows);
+        $this->getTotalWrappingPaper();
+
         return $this->total;
     }
 
     public function generatePart2(array $rows): string
     {
+        $this->initInput($rows);
+        $this->getTotalRibbonPaper();
+
         return $this->total;
     }
 
+    private function initInput(array $rows): void
+    {
+        foreach ($rows as $row) {
+            [$length, $width, $height] = explode('x', $row);
+            $this->packages[] = [
+                'length' => $length,
+                'width' => $width,
+                'height' => $height,
+            ];
+        }
+    }
+
+    /**
+     * input rows must contain {nr}x{nr}x{nr}
+     */
     public function isValidInput(array $rows): bool
     {
-        // TODO: Implement isValidInput() method.
+        foreach ($rows as $row) {
+            preg_match('/^(\d+x\d+x\d+)$/', $row, $matches);
+            if (empty($matches)) {
+                return false;
+            }
+        }
         return true;
+    }
+
+    private function getTotalWrappingPaper(): void
+    {
+        foreach ($this->packages as $package) {
+            $sides = $this->calculateSideWrappingPaper($package);
+            $this->calculateTotalWrappingPaper($sides);
+        }
+    }
+
+    /**
+     * formula: length * width
+     * formula: width * height
+     * formula: height * length
+     *
+     * @return array<int>
+     */
+    private function calculateSideWrappingPaper(array $package): array
+    {
+        return [
+            $package['length'] * $package['width'],
+            $package['width'] * $package['height'],
+            $package['height'] * $package['length'],
+        ];
+    }
+
+    /**
+     * formula: ((lengthWidthSurface + widthHeightSurface + heightLengthSurface) * 2) + smallestSideSurface
+     */
+    private function calculateTotalWrappingPaper(array $sides): void
+    {
+        $this->total += (array_sum($sides) * 2) + min($sides);
+    }
+
+    private function getTotalRibbonPaper(): void
+    {
+        foreach ($this->packages as $package) {
+            $sides = $this->calculateRibbonPaper($package);
+            $bow = $this->calculateRibbonBowPaper($package);
+            $this->total += $sides + $bow;
+        }
+    }
+
+    /**
+     * Get the 2 smallest sides as side1 and side2
+     * formula: side1 + side1 + side2 + side2
+     */
+    private function calculateRibbonPaper(array $package): int
+    {
+        $values = array_values($package);
+        sort($values);
+        [$side1, $side2] = $values;
+
+        return $side1 + $side1 + $side2 + $side2;
+    }
+
+    /**
+     * formula: length * width * height
+     */
+    private function calculateRibbonBowPaper(array $package): int
+    {
+        return $package['length'] * $package['width'] * $package['height'];
     }
 }
