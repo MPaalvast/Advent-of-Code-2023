b87f9a2f77bac1948cf96a09b0caf501728c60d6
MPaalvast <martijn_paalvast@hotmail.com>
2025-01-05 17:41:14 +0100

    Year 2015 Day 4 done

--- src/Service/Days/Year2015/D4Service.php
+++ src/Service/Days/Year2015/D4Service.php
@@ -9,15 +9,40 @@ use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 class D4Service implements DayServiceInterface
 {
     private int $total = 0;
+    private string $preferredPrefix = '';
+    private string $input = '';
 
     public function generatePart1(array $rows): string
     {
+        $this->preferredPrefix = '00000';
+        $this->initInput($rows);
+        $this->findKey();
         return $this->total;
     }
 
     public function generatePart2(array $rows): string
     {
+        $this->preferredPrefix = '000000';
+        $this->initInput($rows);
+        $this->findKey();
         return $this->total;
+    }
+
+    private function initInput(array $rows): void
+    {
+        $this->input = $rows[0];
+    }
+
+    private function findKey(): void
+    {
+        $i = 1;
+        while (true) {
+            if (str_starts_with(md5($this->input . $i), $this->preferredPrefix)) {
+                $this->total = $i;
+                break;
+            }
+            $i++;
+        }
     }
 
     public function isValidInput(array $rows): bool
