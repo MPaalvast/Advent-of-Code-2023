33bf9577be6109dfeefd0c373ba406ef1b3e3e25
MartijnPaalvast <m.paalvast@paragin.com>
2025-04-11 13:55:49 +0200

    removed tailwind changed design a little

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 - composer install
 - php bin/console tailwind:init
 - php bin/console tailwind:build --watch
+- 
+- symfony console asset-map:compile
 
 ##  Database acties
+- symfony console doctrine:database:drop --force
 - symfony console doctrine:database:create
 - symfony console doctrine:migrations:migrate
 
--- config/bundles.php
+++ config/bundles.php
@@ -16,7 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
-    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
