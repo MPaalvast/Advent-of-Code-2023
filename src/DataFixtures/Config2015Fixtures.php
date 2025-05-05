80902fe88ab1ade133ddab2f3926ea02d0fd20b3
MPaalvast <martijn_paalvast@hotmail.com>
2025-05-05 20:49:40 +0200

    Day 9 completed

--- src/DataFixtures/Config2015Fixtures.php
+++ src/DataFixtures/Config2015Fixtures.php
@@ -45,9 +45,9 @@ class Config2015Fixtures extends Fixture implements DependentFixtureInterface
         yield ['day' => $this->getReference('day_6', Day::class), 'title' => "Probably a Fire Hazard", 'active' => true, 'examples' => $this->examples->getDay6Examples(), 'results' => $this->gameDayPartResultIterator(6)];
         yield ['day' => $this->getReference('day_7', Day::class), 'title' => "Some Assembly Required", 'active' => true, 'examples' => $this->examples->getDay7Examples(), 'results' => $this->gameDayPartResultIterator(7)];
         yield ['day' => $this->getReference('day_8', Day::class), 'title' => "Matchsticks", 'active' => true, 'examples' => $this->examples->getDay8Examples(), 'results' => $this->gameDayPartResultIterator(8)];
-        yield ['day' => $this->getReference('day_9', Day::class), 'title' => "All in a Single Night", 'active' => false, 'examples' => $this->examples->getDay9Examples(), 'results' => $this->gameDayPartResultIterator(9)];
-        yield ['day' => $this->getReference('day_10', Day::class), 'title' => "Elves Look, Elves Say", 'active' => false, 'examples' => $this->examples->getDay10Examples(), 'results' => $this->gameDayPartResultIterator(10)];
-        yield ['day' => $this->getReference('day_11', Day::class), 'title' => "Corporate Policy", 'active' => false, 'examples' => $this->examples->getDay11Examples(), 'results' => $this->gameDayPartResultIterator(11)];
+        yield ['day' => $this->getReference('day_9', Day::class), 'title' => "All in a Single Night", 'active' => true, 'examples' => $this->examples->getDay9Examples(), 'results' => $this->gameDayPartResultIterator(9)];
+        yield ['day' => $this->getReference('day_10', Day::class), 'title' => "Elves Look, Elves Say", 'active' => true, 'examples' => $this->examples->getDay10Examples(), 'results' => $this->gameDayPartResultIterator(10)];
+        yield ['day' => $this->getReference('day_11', Day::class), 'title' => "Corporate Policy", 'active' => true, 'examples' => $this->examples->getDay11Examples(), 'results' => $this->gameDayPartResultIterator(11)];
         yield ['day' => $this->getReference('day_12', Day::class), 'title' => "JSAbacusFramework.io", 'active' => false, 'examples' => $this->examples->getDay12Examples(), 'results' => $this->gameDayPartResultIterator(12)];
         yield ['day' => $this->getReference('day_13', Day::class), 'title' => "Knights of the Dinner Table", 'active' => false, 'examples' => $this->examples->getDay13Examples(), 'results' => $this->gameDayPartResultIterator(13)];
         yield ['day' => $this->getReference('day_14', Day::class), 'title' => "Reindeer Olympics", 'active' => false, 'examples' => $this->examples->getDay14Examples(), 'results' => $this->gameDayPartResultIterator(14)];
@@ -75,11 +75,11 @@ class Config2015Fixtures extends Fixture implements DependentFixtureInterface
             case 6:
             case 7:
             case 8:
+            case 9:
+            case 10:
                 yield ['part' => $this->getReference('dayPart_1', DayPart::class), 'solved' => true];
                 yield ['part' => $this->getReference('dayPart_2', DayPart::class), 'solved' => true];
                 break;
-            case 9:
-            case 10:
             case 11:
             case 12:
             case 13:
