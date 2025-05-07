4a6290980205a9d5269f57fc03e2c404666c8f61
MPaalvast <martijn_paalvast@hotmail.com>
2025-05-07 21:21:44 +0200

    Day 15 completed

--- src/DataFixtures/DayExamples/Examples2015.php
+++ src/DataFixtures/DayExamples/Examples2015.php
@@ -702,7 +702,13 @@ EOD
 
     public function day15ExamplesPart2(): array
     {
-        return [];
+        return [
+            ['input' => <<<'EOD'
+Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+EOD
+                , 'result' => 57600000],
+        ];
     }
 
     public function day16ExamplesPart1(): array
