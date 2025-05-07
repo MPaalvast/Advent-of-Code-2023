59e376783e3d643ee8b2776dc230e1590b3dc9a0
MPaalvast <martijn_paalvast@hotmail.com>
2025-05-07 21:21:44 +0200

    Day 15 completed

--- src/DataFixtures/Config2015Fixtures.php
+++ src/DataFixtures/Config2015Fixtures.php
@@ -51,7 +51,7 @@ class Config2015Fixtures extends Fixture implements DependentFixtureInterface
         yield ['day' => $this->getReference('day_12', Day::class), 'title' => "JSAbacusFramework.io", 'active' => true, 'examples' => $this->examples->getDay12Examples(), 'results' => $this->gameDayPartResultIterator(12)];
         yield ['day' => $this->getReference('day_13', Day::class), 'title' => "Knights of the Dinner Table", 'active' => true, 'examples' => $this->examples->getDay13Examples(), 'results' => $this->gameDayPartResultIterator(13)];
         yield ['day' => $this->getReference('day_14', Day::class), 'title' => "Reindeer Olympics", 'active' => true, 'examples' => $this->examples->getDay14Examples(), 'results' => $this->gameDayPartResultIterator(14)];
-        yield ['day' => $this->getReference('day_15', Day::class), 'title' => "Science for Hungry People", 'active' => false, 'examples' => $this->examples->getDay15Examples(), 'results' => $this->gameDayPartResultIterator(15)];
+        yield ['day' => $this->getReference('day_15', Day::class), 'title' => "Science for Hungry People", 'active' => true, 'examples' => $this->examples->getDay15Examples(), 'results' => $this->gameDayPartResultIterator(15)];
         yield ['day' => $this->getReference('day_16', Day::class), 'title' => "Aunt Sue", 'active' => false, 'examples' => $this->examples->getDay16Examples(), 'results' => $this->gameDayPartResultIterator(16)];
         yield ['day' => $this->getReference('day_17', Day::class), 'title' => "No Such Thing as Too Much", 'active' => false, 'examples' => $this->examples->getDay17Examples(), 'results' => $this->gameDayPartResultIterator(17)];
         yield ['day' => $this->getReference('day_18', Day::class), 'title' => "Like a GIF For Your Yard", 'active' => false, 'examples' => $this->examples->getDay18Examples(), 'results' => $this->gameDayPartResultIterator(18)];
@@ -81,10 +81,10 @@ class Config2015Fixtures extends Fixture implements DependentFixtureInterface
             case 12:
             case 13:
             case 14:
+            case 15:
                 yield ['part' => $this->getReference('dayPart_1', DayPart::class), 'solved' => true];
                 yield ['part' => $this->getReference('dayPart_2', DayPart::class), 'solved' => true];
                 break;
-            case 15:
             case 16:
             case 17:
             case 18:
--- src/Service/Days/Year2015/D15Service.php
+++ src/Service/Days/Year2015/D15Service.php
@@ -9,20 +9,113 @@ use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 class D15Service implements DayServiceInterface
 {
     private int $total = 0;
+    private string $caloriesProperty = 'calories';
+    private int $caloriesLimit = 0;
+    private int $spoons = 100;
+    private array $input = [];
+    private array $properties = [];
 
     public function generatePart1(array $rows): string
     {
+        $this->initInput($rows);
+        $this->generateTotal();
+
         return $this->total;
     }
 
     public function generatePart2(array $rows): string
     {
+        $this->initInput($rows);
+        $this->caloriesLimit = 500;
+        $this->generateTotal();
+
         return $this->total;
+    }
+
+    private function generateTotal(): void
+    {
+        for ($z=0;$z<=$this->spoons;$z++) {
+            for ($y=0;$y<=$this->spoons-$z;$y++) {
+                for ($x=0;$x<=$this->spoons-$z-$y;$x++) {
+                    $w = $this->spoons-$x-$y-$z;
+                    $values = [$z,$y,$x,$w];
+
+                    $score = [];
+                    foreach (array_keys($this->properties) as $propertyKey) {
+                        $score[] = $this->calculateResult($propertyKey, $values);
+                    }
+
+                    $scoreResult = 1;
+                    foreach ($score as $scoreItem) {
+                        if ($scoreItem <= 0) {
+                            continue(2);
+                        }
+                    }
+
+                    foreach ($score as $key => $scoreItem) {
+                        if ($this->properties[$key] !== $this->caloriesProperty) {
+                            $scoreResult *= $scoreItem;
+                        } elseif ($this->caloriesLimit > 0 && $scoreItem !== $this->caloriesLimit) {
+                            continue(2);
+                        }
+                    }
+
+                    if ($scoreResult > $this->total) {
+                        $this->total = $scoreResult;
+                    }
+                }
+            }
+        }
+    }
+
+    private function calculateResult(int $nr, array $values): int
+    {
+        $totalRows = count($this->input);
+        $result = 0;
+
+        for ($i=0;$i<$totalRows;$i++) {
+            $result += $this->input[$i][$nr]*$values[$i];
+        }
+
+        return $result;
+    }
+
+    private function initInput(array $rows): void
+    {
+        foreach ($rows as $key => $line) {
+            if (preg_match('/^\w+: \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+)$/', $line, $matches)) {
+                [$full, $value1, $value2, $value3, $value4, $value5] = $matches;
+                $this->input[] = [
+                    (int)$value1,
+                    (int)$value2,
+                    (int)$value3,
+                    (int)$value4,
+                    (int)$value5,
+                ];
+
+                if ($key === 0) {
+                    preg_match('/^\w+: (\w+) -?\d+, (\w+) -?\d+, (\w+) -?\d+, (\w+) -?\d+, (\w+) -?\d+$/', $rows[0], $matches);
+                    [$full, $property1, $property2, $property3, $property4, $property5] = $matches;
+                    $this->properties = [
+                        $property1,
+                        $property2,
+                        $property3,
+                        $property4,
+                        $property5,
+                    ];
+                }
+            }
+        }
     }
 
     public function isValidInput(array $rows): bool
     {
-        // TODO: Implement isValidInput() method.
+        foreach ($rows as $row) {
+            preg_match('/^\w+: \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+)$/', $row, $matches);
+            if (empty($matches)) {
+                return false;
+            }
+        }
         return true;
     }
 }
