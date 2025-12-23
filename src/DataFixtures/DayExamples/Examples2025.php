4e87c22aeb58a87797345404cecc419a9765019d
MPaalvast <martijn_paalvast@hotmail.com>
2025-12-23 17:00:36 +0100

    Add day fixtures 2025

---
+++ src/DataFixtures/DayExamples/Examples2025.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\DataFixtures\DayExamples;
+
+class Examples2025
+{
+    public function day1ExamplesPart1(): array
+    {
+        return [
+            ['input' => <<<'EOT'
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+EOT, 'result' => 3],
+        ];
+    }
+
+    public function day1ExamplesPart2(): array
+    {
+        return $this->day1ExamplesPart1();
+    }
+
+    public function day2ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day2ExamplesPart2(): array
+    {
+        return $this->day2ExamplesPart1();
+    }
+
+    public function day3ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day3ExamplesPart2(): array
+    {
+        return $this->day3ExamplesPart1();
+    }
+
+    public function day4ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day4ExamplesPart2(): array
+    {
+        return $this->day4ExamplesPart1();
+    }
+
+    public function day5ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day5ExamplesPart2(): array
+    {
+        return $this->day5ExamplesPart1();
+    }
+
+    public function day6ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day6ExamplesPart2(): array
+    {
+        return $this->day6ExamplesPart1();
+    }
+
+    public function day7ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day7ExamplesPart2(): array
+    {
+        return $this->day7ExamplesPart1();
+    }
+
+    public function day8ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day8ExamplesPart2(): array
+    {
+        return $this->day8ExamplesPart1();
+    }
+
+    public function day9ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day9ExamplesPart2(): array
+    {
+        return $this->day9ExamplesPart1();
+    }
+
+    public function day10ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day10ExamplesPart2(): array
+    {
+        return $this->day10ExamplesPart1();
+    }
+
+    public function day11ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day11ExamplesPart2(): array
+    {
+        return $this->day11ExamplesPart1();
+    }
+
+    public function day12ExamplesPart1(): array
+    {
+        return [
+            ['input' => '', 'result' => 0]
+        ];
+    }
+
+    public function day12ExamplesPart2(): array
+    {
+        return $this->day12ExamplesPart1();
+    }
+}
